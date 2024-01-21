22736f9e69bc99cce63aaa4fbc57059dbbfe37e0
Timur Valeev <timurrr@gmail.com>
2024-01-21 21:20:17 +0500

    Welcome controller

--- app/src/main/java/hexlet/code/controller/WelcomeController.java
+++ app/src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,2 +1,13 @@
-package hexlet.code.controller;public class WelcomeController {
+package hexlet.code.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping(path = "")
+    public String welcome() {
+        return "Welcome to Spring!";
+    }
 }
