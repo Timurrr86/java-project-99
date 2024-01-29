9e252b5642144dd69434c1c1a3c81fedb55933b3
Timur Valeev <timurrr@gmail.com>
2024-01-29 23:59:30 +0500

    added user model

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	id("io.freefair.lombok") version "8.4"
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -25,6 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("jakarta.validation:jakarta.validation-api:3.1.0-M1")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("com.h2database:h2:2.2.224")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
---
+++ src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,43 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+
+
+import jakarta.validation.constraints.Email;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    @Email
+    private String email;
+    private String password;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate  updatedAt;
+}
