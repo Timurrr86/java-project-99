d3a8c8f43c969cc499acc581c54ee53d6e435e4a
Timur Valeev <timurrr@gmail.com>
2024-02-18 21:33:08 +0500

    fixed checkstyle error2

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
-	@Bean
-	public Faker getFaker() {
-		return new Faker();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+    @Bean
+    public Faker getFaker() {
+        return new Faker();
+    }
 }
