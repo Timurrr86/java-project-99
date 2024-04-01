2278c7662329eb165673910938b103b093acf47d
Timur Valeev <timurrr@gmail.com>
2024-04-01 21:11:37 +0500

    removed preauthorize in labelcontroller

--- src/main/java/hexlet/code/controller/LabelsController.java
+++ src/main/java/hexlet/code/controller/LabelsController.java
@@ -11,7 +11,6 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -68,7 +67,6 @@ public class LabelsController {
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PreAuthorize("@userUtils.isAuthenticated()")
     void destroyLabel(@PathVariable Long id) {
         labelService.delete(id);
     }
